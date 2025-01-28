3214d8632f4aa8a664360a742c25cd8462a2d01c
Anish parajuli <parajulianish708@gmail.com>
2025-01-29 00:02:15 +0545

    Create pose_detection.py

---
+++ pose_detection.py
@@ -0,0 +1,38 @@
+import cv2
+import mediapipe as mp
+
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(
+    static_image_mode=False,
+    model_complexity=1,
+    smooth_landmarks=True,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
+
+mp_draw = mp.solutions.drawing_utils
+
+def pose_detection():
+    cap = cv2.VideoCapture(0)
+
+    try:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame = cv2.flip(frame, 1)
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = pose.process(frame_rgb)
+            if results.pose_landmarks:
+                mp_draw.draw_landmarks(
+                    frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+            cv2.imshow("Pose Detection", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    pose_detection()
